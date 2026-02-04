bb3a3ad37fb0d9a23df19a919a215bd255553381
Nguyen Luong Cuong <645101k010@st.utc2.edu.vn>
2026-02-04 20:11:39 +0700

    Add files via upload

---
+++ general.js
@@ -0,0 +1,133 @@
+const express = require('express');
+let books = require("./booksdb.js");
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
+const public_users = express.Router();
+const axios = require('axios');
+
+
+public_users.post("/register", (req,res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    if (!isValid(username)) {
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registered. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});
+    }
+  }
+  return res.status(404).json({message: "Unable to register user."});
+});
+
+// Get the book list available in the shop
+public_users.get('/',function (req, res) {
+  //Write your code here
+  return res.status(200).send(JSON.stringify(books, null, 4));
+});
+
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn',function (req, res) {
+  //Write your code here
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+      return res.status(200).json(books[isbn]);
+  } else {
+      return res.status(404).json({message: "Book not found"});
+  }
+ });
+  
+// Get book details based on author
+public_users.get('/author/:author',function (req, res) {
+  //Write your code here
+  const author = req.params.author;
+  const bookKeys = Object.keys(books);
+  const booksByAuthor = [];
+  
+  bookKeys.forEach(key => {
+      if (books[key].author === author) {
+          booksByAuthor.push(books[key]);
+      }
+  });
+  
+  if (booksByAuthor.length > 0) {
+      return res.status(200).json(booksByAuthor);
+  } else {
+      return res.status(404).json({message: "No books found by this author"});
+  }
+});
+
+// Get all books based on title
+public_users.get('/title/:title',function (req, res) {
+  //Write your code here
+  const title = req.params.title;
+  const bookKeys = Object.keys(books);
+  const booksByTitle = [];
+
+  bookKeys.forEach(key => {
+      if (books[key].title === title) {
+          booksByTitle.push(books[key]);
+      }
+  });
+
+  if (booksByTitle.length > 0) {
+      return res.status(200).json(booksByTitle);
+  } else {
+      return res.status(404).json({message: "No books found with this title"});
+  }
+});
+
+//  Get book review
+public_users.get('/review/:isbn',function (req, res) {
+  //Write your code here
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+      return res.status(200).json(books[isbn].reviews);
+  } else {
+      return res.status(404).json({message: "Book not found"});
+  }
+});
+
+module.exports.general = public_users;
+
+// Task 10: Get the list of books available in the shop using async-await
+const getAllBooks = async () => {
+    try {
+        const response = await axios.get("http://localhost:5000/");
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// Task 11: Get book details based on ISBN using Promises
+const getBookByISBN = (isbn) => {
+    axios.get(`http://localhost:5000/isbn/${isbn}`)
+    .then(response => {
+        console.log(response.data);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+}
+
+// Task 12: Get book details based on Author using async-await
+const getBookByAuthor = async (author) => {
+    try {
+        const response = await axios.get(`http://localhost:5000/author/${author}`);
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// Task 13: Get book details based on Title using async-await
+const getBookByTitle = async (title) => {
+    try {
+        const response = await axios.get(`http://localhost:5000/title/${title}`);
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
